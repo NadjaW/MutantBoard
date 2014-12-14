4df71a8a6ff227fc6527e220bdd26a74ffa9e38e
nwalter <nwalter@suse.de>
2014-12-14 13:07:59 +0100

    add route for mutant controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -27,6 +27,11 @@ Rails.application.routes.draw do
   #       get 'sold'
   #     end
   #   end
+
+  Rails.application.routes.draw do
+    resources :mutants
+    root 'welcome#index'
+  end
 
   # Example resource route with sub-resources:
   #   resources :products do
