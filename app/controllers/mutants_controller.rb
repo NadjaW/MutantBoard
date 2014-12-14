9749dcd67f66cca4632425fc2f9ce93fb036a280
nwalter <nwalter@suse.de>
2014-12-14 14:38:26 +0100

    add validation

--- app/controllers/mutants_controller.rb
+++ app/controllers/mutants_controller.rb
@@ -1,13 +1,22 @@
 class MutantsController < ApplicationController
 
   def new
+    # we added @mutant = Mutant.new in the MutantController is that otherwise @mutant would be nil in our view,
+    # and calling @mutant.errors.any? (mutants/new.html.erb) would throw an error.
+    @mutant = Mutant.new
   end
 
   def create
-    #render plain: params[:mutant].inspect
+    # render plain: params[:mutant].inspect
     @mutant = Mutant.new(mutant_params)
-    @mutant.save
-    redirect_to @mutant
+
+    if @mutant.save
+      redirect_to @mutant
+    else
+      # The render method is used so that the @mutant object is passed back to the new template when it is rendered.
+      # This rendering is done within the same request as the form submission, whereas the redirect_to will tell the browser to issue another request.
+      render 'new'
+    end
   end
 
   def show
--- app/models/mutant.rb
+++ app/models/mutant.rb
@@ -1,2 +1,10 @@
 class Mutant < ActiveRecord::Base
+  validates :name, presence: true,
+                   length: { minimum: 3 }
+
+  validates :superpower, presence: true,
+                         length: { minimum: 3 }
+
+  validates :cv, presence: true,
+                 length: { minimum: 3 }
 end
