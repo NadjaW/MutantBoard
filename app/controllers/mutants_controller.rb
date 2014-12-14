e5f8a1a54087a8c1db2f2ae17be03f5b820ed301
nwalter <nwalter@suse.de>
2014-12-14 13:52:38 +0100

    create mutant, save in database and show him or her in browser

--- app/controllers/mutants_controller.rb
+++ app/controllers/mutants_controller.rb
@@ -3,4 +3,20 @@ class MutantsController < ApplicationController
   def new
   end
 
+  def create
+    #render plain: params[:mutant].inspect
+    @mutant = Mutant.new(mutant_params)
+    @mutant.save
+    redirect_to @mutant
+  end
+
+  def show
+    @mutant = Mutant.find(params[:id])
+  end
+
+  private
+    def mutant_params
+      params.require(:mutant).permit(:name, :superpower, :cv)
+    end
+
 end
